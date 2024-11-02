f0f7d11ec9dca231ca8e8241d1760a2ffc55bcc9
Jakub Krawiec <kubakrawieckk04@gmail.com>
2024-11-02 20:37:11 +0100

    Setting up folders structure, libraries has been installed

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,14 @@
+import { Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+
 function App() {
-  return <></>;
+  return (
+    <>
+      <Routes>
+        <Route path="/" element={<Home />}></Route>
+      </Routes>
+    </>
+  );
 }
 
 export default App;
