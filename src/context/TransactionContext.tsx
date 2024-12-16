ad3d2e4eae03a6b8e225b07d4ea1856bc67195c6
Jakub Krawiec <kubakrawieckk04@gmail.com>
2024-12-16 17:28:12 +0100

    Fixed problems in Transaction Context

--- src/context/TransactionContext.tsx
+++ src/context/TransactionContext.tsx
@@ -1,7 +1,7 @@
 import { useFetchCurrentUserExpenses } from "@/hooks/useFetchCurrentUserExpenses";
 import { useFetchCurrentUserRevenues } from "@/hooks/useFetchCurrentUserRevenues";
 import { useUser } from "@/hooks/useUser";
-import { TransactionContextType, TransactionType } from "@/types";
+import { TransactionContextType } from "@/types";
 import { createContext, ReactNode } from "react";
 
 const initialValue = {
